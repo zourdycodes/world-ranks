2824dbafbca1cecec8cc818cd169512d0beaa875
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-30 19:25:35 +0700

    :adhesive_bandage: fixing non-critical issue

--- components/CountryTable/index.jsx
+++ components/CountryTable/index.jsx
@@ -1,19 +1,42 @@
+import { KeyboardArrowDownRounded } from "@material-ui/icons";
 import styles from "./CountryTable.module.css";
 
+const orderBy = (countries = [], direction) => {
+  if (direction === "asc") {
+    return [...countries].sort((a, b) =>
+      a.population > b.population ? 1 : -1
+    );
+  }
+
+  if (direction === "desc") {
+    return [...countries].sort((a, b) =>
+      a.population > b.population ? -1 : 1
+    );
+  }
+
+  return countries;
+};
+
 export const CountryTable = ({ countries }) => {
+  const orderedByPopulation = orderBy(countries, "desc");
+
   return (
     <div>
       <div className={styles.heading}>
         <button className={styles.heading_name}>
           <div>Name</div>
+
+          <KeyboardArrowDownRounded />
         </button>
 
         <button className={styles.heading_population}>
           <div>Population</div>
+
+          <KeyboardArrowDownRounded />
         </button>
       </div>
 
-      {countries?.map((country) => (
+      {orderedByPopulation?.map((country) => (
         <div key={country.numericCode} className={styles.row}>
           <div className={styles.name}>{country.name}</div>
           <div className={styles.population}>{country.population}</div>
